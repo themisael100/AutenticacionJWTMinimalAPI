813222b9ec40cdc6b9f07aa0ec54145219deae3b
Bryan Misael Sanchez Ramirez <themisael1000@hotmail.com>
2023-09-12 19:54:39 -0600

    Configuracion de la clase TestEndpoint

---
+++ AutenticacionJWTMinimalAPI/Endpoints/TestEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace AutenticacionJWTMinimalAPI.Endpoints
+{
+    public static class TestEndpoint
+    {
+        static List<Object> data = new List<object>();
+
+        public static void AddTestEndpints(this WebApplication app)
+        {
+            app.MapGet("/test", () =>
+            {
+                return data;
+            }).AllowAnonymous();
+
+            app.MapPost("/test", (string name, string lastname) =>
+            {
+                data.Add(new { name, lastname });
+                return Results.Ok();
+            }).AllowAnonymous();
+
+            app.MapDelete("/test", () =>
+            {
+                data = new List<object>();
+
+                return Results.Ok();
+            }).RequireAuthorization();
+        }
+    }
+}
