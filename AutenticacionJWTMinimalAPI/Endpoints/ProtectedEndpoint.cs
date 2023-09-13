699bd8240c774ace5bbde20c1fc75c570192ebb6
Bryan Misael Sanchez Ramirez <themisael1000@hotmail.com>
2023-09-12 19:47:43 -0600

    Configuracion de la clase ProtectedEndpoint

---
+++ AutenticacionJWTMinimalAPI/Endpoints/ProtectedEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace AutenticacionJWTMinimalAPI.Endpoints
+{
+    public static class ProtectedEndpoint
+    {
+        static List<Object> data = new List<object>();
+
+        public static void AddProtectedEndpoints(this WebApplication app)
+        {
+            app.MapGet("/protected", () =>
+            {
+                return data;
+            }).RequireAuthorization();
+
+            app.MapPost("/protected", (string name, string lastname) =>
+            {
+                data.Add(new { name, lastname });
+
+                return Results.Ok();
+            }).RequireAuthorization();
+        }
+    }
+}
